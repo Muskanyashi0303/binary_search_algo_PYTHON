1635b953cf8480e14fdb08fdb4d7919d5bbd7b2d
Muskanyashi0303 <82176595+Muskanyashi0303@users.noreply.github.com>
2021-06-20 18:49:10 +0530

    Add files via upload

---
+++ Binary_Search_Algorithm.py
@@ -0,0 +1,46 @@
+# Muskan Gupta
+
+
+def binary_search(arr, low, high, x):
+
+    if high >= low:
+
+        mid = (high + low) // 2
+
+        if arr[mid] == x:
+            return mid
+
+        elif arr[mid] > x:
+            return binary_search(arr, low, mid - 1, x)
+
+        else:
+            return binary_search(arr, mid + 1, high, x)
+
+    else:
+
+        return -1
+
+
+Arr = input('Enter the elements of array: ')
+arr_i = Arr.split(' ')
+arr = []
+for i in arr_i:
+    arr.append(int(i))
+key = int(input('Enter the number to find: '))
+arr.sort()
+print('Sorted Array:')
+print(arr)
+
+
+index = binary_search(arr, 0, len(arr)-1, key)
+
+if index != -1:
+    print(f"Successfully!! Element is present in the array at index {index}")
+else:
+    print("Element is not present in array!!")
+
+
+
+
+
+
